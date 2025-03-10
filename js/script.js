f3474a577b81f2ed3d591bd1056d25ddf1d2fa0d
osama <obg1309@gmail.com>
2025-03-10 19:13:56 +0100

    La parte de JS del ejercicio terminada

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
  // Aquí tu código
+ const agregar = document.getElementById('agregar');
+ const lista = document.getElementById('lista');
+ 
+ agregar.addEventListener('click', function() {
+   const elmentoNuevo = prompt('Elemento que quieres agregar a la lista: ');
+   lista.innerHTML += '<li>' + elmentoNuevo + '</li>';  
+ });
